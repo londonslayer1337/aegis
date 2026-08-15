0619e49fecae3014437d9f614ef35f7f5250d875
londonslayer1337 <qwwxeky@gmail.com>
2026-08-15 14:50:10 +0300

    Update warp_generator.py

--- warp_generator.py
+++ warp_generator.py
@@ -124,6 +124,30 @@ COUNTRY_ENDPOINTS = {
     "nl": {
         "name": "🇳🇱 Нидерланды",
         "clean_ips": [
+import base64
+import random
+import httpx
+from cryptography.hazmat.primitives.asymmetric import x25519
+from cryptography.hazmat.primitives import serialization
+
+HEADERS = {
+    "User-Agent": "okhttp/3.12.1",
+    "Content-Type": "application/json; charset=UTF-8"
+}
+
+# Пулы Clean IP Cloudflare по странам
+COUNTRY_ENDPOINTS = {
+    "de": {
+        "name": "🇩🇪 Германия",
+        "clean_ips": [
+            "162.159.192.1", "162.159.192.2", "162.159.192.3", "162.159.192.4", "162.159.192.5",
+            "162.159.192.8", "162.159.192.10", "162.159.193.1", "162.159.193.2", "162.159.193.3",
+            "162.159.193.4", "162.159.193.5", "162.159.193.8", "162.159.193.10"
+        ]
+    },
+    "nl": {
+        "name": "🇳🇱 Нидерланды",
+        "clean_ips": [
             "188.114.96.1", "188.114.96.2", "188.114.96.3", "188.114.96.4", "188.114.96.5",
             "188.114.96.8", "188.114.96.10", "188.114.97.1", "188.114.97.2", "188.114.97.3",
             "188.114.97.4", "188.114.97.5", "188.114.97.8", "188.114.97.10"
